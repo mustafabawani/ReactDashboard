f9a5bbaab0db256c73a10ad94e47dd7d7a411ca2
mustafa <mustafa_bawani@hotmail.com>
2023-05-16 04:34:31 +0500

    added delete button to chart cards

--- src/Analysis.js
+++ src/Analysis.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 import styled from "styled-components";
 import { parse } from 'papaparse';
 import FileInput from './components/FileInput';
-import { Button,Tooltip,TextField,MenuItem } from '@mui/material';
+import { Button, Tooltip, TextField, MenuItem } from '@mui/material';
 import BarChartIcon from "@mui/icons-material/BarChart";
 import PieChartIcon from "@mui/icons-material/PieChart";
 import ShowChart from "@mui/icons-material/ShowChart";
 import Input from './components/Input';
 import BarChart from './components/Charts/BarChart';
 import LineChart from './components/Charts/LineGraph';
+import DeleteIcon from '@mui/icons-material/Delete';
 import PieChart from './components/Charts/PieChart';
 
 const Container = styled.div`
@@ -61,6 +62,32 @@ const RightBar3 = styled.div`
   flex-direction: column;
 `;
 
+const ChartWrapper = styled.div`
+  width: 45%;
+  height: 300px;
+  margin-bottom: 5px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.15);
+  padding: 10px;
+  background-color: #f9f9f9;
+  position: relative;
+  overflow: hidden;
+
+  &:hover .deleteButton {
+    opacity: 1;
+  }
+`;
+
+const DeleteButton = styled(DeleteIcon)`
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  cursor: pointer;
+  opacity: 0;
+  transition: opacity 0.2s;
+`;
+
 const Analysis = () => {
   const [dataColumn, setDataColumn] = useState([]);
   const [data, setData] = useState([]);
@@ -79,42 +106,41 @@ const Analysis = () => {
       setDataColumn(columns);
       setData(dataRows);
     };
-    
+
     reader.readAsText(file);
   };
-  const formatDataForCharts = (data, xAxis, yAxis) => {
-    let dataForX = [];
-    let dataForY = [];
-  
-    data.forEach(item => {
-      dataForX.push(item[xAxis]);
-      dataForY.push(item[yAxis]);
-    });
-    
-    return {
-      labels: dataForX,
-      datasets: [{
-        label: yAxis,
-        data: dataForY,
-        backgroundColor: [
-          "#00a4ec",
-          "#ff567b",
-          "#00c3c3",
-          "#ff9a33",
-          "#ffcb53",
-          "#a164fd",
-        ],
-        borderColor: '#00001a',
-        borderWidth: 1,
-      }]
-    }
-  };
-  
-  
-  const handleCreate = () => {
-    const chartData = formatDataForCharts(data, xAxis, yAxis);
-  
-    setCharts(prevCharts => [
+
+  const formatDataForCharts = (data,
+    xAxis, yAxis) => {
+      let dataForX = [];
+      let dataForY = [];
+      
+      data.forEach(item => {
+        dataForX.push(item[xAxis]);
+        dataForY.push(item[yAxis]);
+      });
+      
+      return {
+        labels: dataForX,
+        datasets: [{
+          label: yAxis,
+          data: dataForY,
+          backgroundColor: [
+            "#00a4ec",
+            "#ff567b",
+            "#00c3c3",
+            "#ff9a33",
+            "#ffcb53",
+            "#a164fd",
+          ],
+          borderColor: '#00001a',
+          borderWidth: 1,
+        }]
+      };
+    };
+
+    const handleCreate = () => {
+    const chartData = formatDataForCharts(data, xAxis, yAxis);setCharts(prevCharts => [
       ...prevCharts,
       {
         type: chartType,
@@ -123,87 +149,81 @@ const Analysis = () => {
     ]);
   };
 
-
-  return (
+  const handleDelete = (indexToDelete) => {
+  setCharts(charts.filter((_, index) => index !== indexToDelete));
+  };return (
     <Container>
-      <LeftBar>
-      {charts.map((chart, index) => {
-        const chartStyle = {
-          width: '45%', 
-          height: '300px', 
-          marginBottom: '5px', 
-          border: '1px solid #ddd',
-          borderRadius: '5px',
-          boxShadow: '0 2px 5px rgba(0, 0, 0, 0.15)', // add a subtle shadow
-          padding: '10px', // add some padding
-          backgroundColor: '#f9f9f9', // a light background color
-        };
-        switch (chart.type) {
-          case 'Bar':
-            return(
-              <div key={index} style={chartStyle}>
-              <BarChart key={index} chartData={chart.data} />
-              </div>
-              );
-          case 'Pie':
-            return(
-              <div key={index} style={chartStyle}>
-                <PieChart key={index} chartData={chart.data} />
-              </div>
-              );
-          case 'Line':
-            return(
-              <div key={index} style={chartStyle}>
-               <LineChart key={index} chartData={chart.data} />
-              </div>
-              );
-          default:
-            return null;
-        }
-      })}
-      </LeftBar>
-      <RightBar>
-        <RightBar1>
-          <h3 style={{color:"white"}}>CSV File Importer</h3>
-          <FileInput onFileChange={handleFileChange} />
-        </RightBar1>
-        <RightBar2>
-          <h3 style={{color:"white"}}>CUSTOMIZE CHART TYPE</h3>
-          <form className="createProjectForm">
-            <div style={{ display: "flex", flexDirection: "row" }}>
-              <Tooltip title="Bar Graph" placement="top">
-                <BarChartIcon
-                  style={{ cursor: "pointer", marginLeft: "2%",
-                  color: chartType === "Bar" ? "blue" : "white"  }}
-                  onClick={() => setChartType("Bar")}
-                />
-              </Tooltip>
-              <Tooltip title="Pie Chart" placement="top">
-                <PieChartIcon
-                  style={{ cursor: "pointer", marginLeft: "2%",
-                  color: chartType === "Pie" ? "blue" : "white"  }}
-                  onClick={() => setChartType("Pie")}
-                />
-              </Tooltip>
-              <Tooltip title="Line Chart" placement="top">
-                <ShowChart
-                  style={{ cursor: "pointer", marginLeft: "2%",
-                  color: chartType === "Line" ? "blue" : "white"  }}
-                  onClick={() => setChartType("Line")}
-                />
-              </Tooltip>
-            </div>
-          </form>
-        </RightBar2>
-        <RightBar3>
-          <Input text='X-axis' data={dataColumn} setValue={setXAxis}/>
-          <Input text="Y-axis" data={dataColumn} setValue={setYAxis}/>
-          <Button style={{marginTop:"3%",color:"white"}} onClick={handleCreate}>Create</Button>
-        </RightBar3>
-      </RightBar>
-    </Container>
-  );
+    <LeftBar>
+    {charts.map((chart, index) => (
+    <ChartWrapper key={index}>
+    {chart.type === 'Bar' && (
+    <BarChart key={index} chartData={chart.data} />
+    )}
+    {chart.type === 'Pie' && (
+    <PieChart key={index} chartData={chart.data} />
+    )}
+    {chart.type === 'Line' && (
+    <LineChart key={index} chartData={chart.data} />
+    )}
+    <DeleteButton className="deleteButton" onClick={() => handleDelete(index)} />
+    </ChartWrapper>
+    ))}
+    </LeftBar>
+    <RightBar>
+    <RightBar1>
+    <h3 style={{ color: "white" }}>CSV File Importer</h3>
+    <FileInput onFileChange={handleFileChange} />
+    </RightBar1>
+    <RightBar2>
+    <h3 style={{ color: "white" }}>CUSTOMIZE CHART TYPE</h3>
+    <form className="createProjectForm">
+    <div style={{ display: "flex", flexDirection: "row" }}>
+    <Tooltip title="Bar Graph" placement="top">
+    <BarChartIcon
+    style={{
+    cursor: "pointer",
+    marginLeft: "2%",
+    color: chartType === "Bar" ? "blue" : "white"
+    }}
+    onClick={() => setChartType("Bar")}
+    />
+    </Tooltip>
+    <Tooltip title="Pie Chart" placement="top">
+    <PieChartIcon
+    style={{
+    cursor: "pointer",
+    marginLeft: "2%",
+    color: chartType === "Pie" ? "blue" : "white"
+    }}
+    onClick={() => setChartType("Pie")}
+    />
+    </Tooltip>
+    <Tooltip title="Line Chart" placement="top">
+    <ShowChart
+    style={{
+    cursor: "pointer",
+    marginLeft: "2%",
+    color: chartType === "Line" ? "blue" : "white"
+    }}
+    onClick={() => setChartType("Line")}
+    />
+    </Tooltip>
+    </div>
+    </form>
+    </RightBar2>
+    <RightBar3>
+    <Input text='X-axis' data={dataColumn} setValue={setXAxis} />
+    <Input text="Y-axis" data={dataColumn} setValue={setYAxis} />
+    <Button
+    style={{ margin: "3%", color: "white", backgroundColor: "blue" }}
+    onClick={handleCreate}
+    >
+    Create
+    </Button>
+    </RightBar3>
+  </RightBar>
+</Container>
+);
 }
 
-export default Analysis;
-
+export default Analysis;                
